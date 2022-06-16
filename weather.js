461bebb986aed5b99c8cf58e64cee138c542d63d
mehmet <tas349873@gmail.com>
2022-06-16 21:41:29 +0300

    v2

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ const list = document.querySelector(".ajax-section .cities");
 localStorage.setItem("apiKey",EncryptStringAES("3ee3ae18c0990b250806b50911aa52c6"))
 
 
-form.addEventListener("submit",() => {
+form.addEventListener("submit", async() => {
     e.prevenDefault()
     getWeathenDataFromApi();
 })
@@ -14,5 +14,24 @@ form.addEventListener("submit",() => {
 
 
 const getWeathenDataFromApi = ()=> {
-    alert("http request sended");
+    // alert("http request gone");
+    // input.value = "";
+    let tokenKey =DecryptStringAES(localStorage.getItem("apiKey"));
+    // console.log(apikey);
+    let inputVal = input.value;
+    let unitType = "metric";
+    let lang = "tr";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${tokenKey}&units=${unitType}&lang=${lang}&`;
+  
+
+     try {
+        // const response = await fetch(url).then(response =>response.json());
+        const response = await axios();
+        const{name,main,sys,weather}=response.data;
+        let iconUrl =`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+     } catch (error) {
+        
+     }
+    form.reset();
+
 }
